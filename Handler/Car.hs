1d301e8bdb57d3c8ad1ffa227a9f058457e6d095
Artem Puchenkin <apuchenkin@gmail.com>
2015-03-04 20:27:37 +0300

    cars dynamic filters

--- Handler/Car.hs
+++ Handler/Car.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Handler.Car where
 
 import Import
+import Data.Aeson
 
 instance ToJSON Car where
     toJSON Car {..} = object
@@ -14,15 +17,62 @@ instance ToJSON a => ToJSON (Entity a) where
         , "data" .= toJSON a
         ]
 
+instance FromJSON Car where
+    parseJSON (Object v) = Car
+        <$> v .:? "model"
+        <*> v .:? "year"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _          = mzero
+
 carAForm :: FormInput Handler Car
 carAForm = Car
-    <$> ireq textField "model"
-    <*> ireq intField "year"
+    <$> iopt textField "model"
+    <*> iopt intField "year"
 
 getCarsR :: Handler Value
 getCarsR = do
-    cars <- runDB $ selectList [] [] :: Handler [Entity Car]
+    qs <- lookupGetParam "filter"
+    let filter = join $ decodeStrict <$> (encodeUtf8 <$> qs) :: Maybe Car
+    cars <- runDB $ selectList (toFilter filter) [] :: Handler [Entity Car]
     returnJson $ cars
+    where
+        toFilter :: Maybe Car -> [Filter Car]
+        toFilter car = case car of
+            Nothing            -> []
+            Just Car {..}      ->
+                [] ++ (
+                    case carModel of
+                        Just value -> [CarModel ==. Just value]
+                        Nothing -> []
+                ) ++ (
+                    case carYear  of
+                        Just value -> [CarYear  ==. Just value]
+                        Nothing -> []
+                )
+
+
+--
+--                filter notNull [
+--                    CarModel ==. carModel,
+--                    CarYear ==. carYear
+--                ]
+--
+--        notNull :: Filter Car -> Bool
+--        notNull Filter {} = False
+--        notNull Filter {filterValue = typ} = case typ of
+--            Right _ -> False
+--            Left v -> case v of
+--                Just v -> True
+--                Nothing -> False
+
+--
+--        mapFilter :: PersistField v => (EntityField Car (Maybe v), Maybe v) -> Filter Car
+--        mapFilter (field, value) = field ==. value
+--
+--        notNull :: PersistField v => (EntityField Car (Maybe v), Maybe v) -> Bool
+--        notNull (_, value) = case value of
+--            Just value -> True
+--            Nothing -> False
 
 postCarsR :: Handler Value
 postCarsR = do
@@ -33,4 +83,4 @@ postCarsR = do
 getCarR :: CarId -> Handler Value
 getCarR carId = do
     car <- runDB $ get carId
-    returnJson $ car
+    returnJson car
